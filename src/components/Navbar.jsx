e463e1fd2ff79dfff904844b4d5ec8e9096413f9
Rafael Moreira <rafa.siffert@gmail.com>
2023-09-27 18:09:22 -0400

    tailwind problems with logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,8 @@ const Navbar = () => {
             <img
               src={logo}
               alt="logo"
-              className="w-10 h-10 object-contain"
+              className="w-10 h-10 object-fill"    
+              //  why arent the styles being appleid here?
             />
           </Link>
         </div>
