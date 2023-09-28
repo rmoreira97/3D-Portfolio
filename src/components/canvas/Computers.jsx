460953a93e031499b4e835c7186155c6cc35c561
Rafael Moreira <rafa.siffert@gmail.com>
2023-09-27 21:04:36 -0400

    gorilla too far away

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,10 +5,16 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./public/desktop_pc/scene.gltf");
+  const computer = useGLTF('public/public/gorilla_scan.glb');
+
+// Define the scale factor for the model
+const scaleFactor = 4; // You can adjust this value to change the size
+
+// Define the Z-coordinate for the new position (closer to the camera)
+const zPosition = isMobile ? 2 : 2; // Adjust this value as needed
 
   return (
-    <mesh>
+    <mesh scale={[scaleFactor, scaleFactor, scaleFactor]}>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
@@ -21,13 +27,13 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={10} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        position={[1, -1, 0]} // Set the new Z-coordinate here
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
 };
+
 
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
